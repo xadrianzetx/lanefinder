50905391830a838c44cb6692aeea3c925f0d62b4
Adrian Zuber <xadrianzetx@gmail.com>
2019-12-04 19:31:20 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ sudo apt-get update
 sudo apt-get -y install libglib2.0-0 libsm6 libxext6 libxrender-dev python3-edgetpu libedgetpu1-std
 
 # install python requirements
+wget https://dl.google.com/coral/python/tflite_runtime-1.14.0-cp35-cp35m-linux_armv7l.whl
 pip3 install -r requirements.txt
